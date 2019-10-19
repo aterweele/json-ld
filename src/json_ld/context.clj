2c819632dc444f09d60e6cc39c21c1b32774f50e
Alex ter Weele <alex.ter.weele@gmail.com>
2019-10-19 15:07:20 -0500

    Implement more context-processing.

--- src/json_ld/context.clj
+++ src/json_ld/context.clj
@@ -1,4 +1,21 @@
-(ns json-ld.context)
+(ns json-ld.context
+  "Implementation of JSON-LD context processing
+  algorithms (https://www.w3.org/TR/json-ld-api/#context-processing-algorithms)."
+  (:require [clojure.string :as str]))
+
+;;; helpers that may be moved to other namespaces
+
+(defn absolute-iri?
+  [iri]
+  (RuntimeException. "TODO implement"))
+
+(defn relative-iri?
+  [iri]
+  (RuntimeException. "TODO implement"))
+
+(defn blank-node-identifier?
+  [s]
+  (RuntimeException. "TODO implement"))
 
 (defn resolve-against
   ;; TODO: is "path" an accurate description?
@@ -6,12 +23,16 @@
   (RuntimeException. "TODO implement"))
 
 (defn dereference [iri]
+  ;; The failure mode of `dereference` is described in (3.2.3):
+
   ;; "(3.2.3) ...If context cannot be dereferenced, a loading remote
   ;; context failed error has been detected and processing is
   ;; aborted. If the dereferenced document has no top-level JSON
   ;; object with an @context member, an invalid remote context has
   ;; been detected and processing is aborted; ..."
   (RuntimeException. "TODO implement"))
+
+(declare create-term-definition)
 
 (defn- contextualize*
   ;; TODO bad name. Impl of 6.1 Context Processing Algorithm.
@@ -96,12 +117,7 @@
                               document-iri
                               (conj remote-contexts context))
               ;; "(3.2.5) Continue with the next context."
-              (recur % rest))
-            (recur (contextualize* result
-                                   (dereference context)
-                                   document-iri
-                                   (conj remote-contexts context))
-                   rest))
+              (recur % rest)))
           ;; "(3.3) If context is not a JSON object, an invalid local
           ;; context error has been detected and processing is
           ;; aborted."
@@ -115,18 +131,66 @@
            ;; "(3.4) If context has an @base key and remote contexts
            ;; is empty, i.e., the currently being processed context is
            ;; not a remote context:"
-           (when (and (contains? context "@base")
-                      (empty? remote-contexts))
-             ;; TODO 3.4.1-5
-             )
+           (when-let [[_ value] (and (empty? remote-contexts)
+                                     (find context "@base"))]
+             ;; "(3.4.1) Initialize value to the value associated with
+             ;; the @base key." (see above)
+             (let [{:keys [base-iri]} result]
+               {:base-iri
+                (cond
+                  ;; "(3.4.2) If value is null, remove the base IRI of
+                  ;; result."
+                  (nil? value) nil
+                  ;; "(3.4.3) Otherwise, if value is an absolute IRI,
+                  ;; the base IRI of result is set to value."
+                  (absolute-iri? value) value
+                  ;; "(3.4.4) Otherwise, if value is a relative IRI and
+                  ;; the base IRI of result is not null, set the base
+                  ;; IRI of result to the result of resolving value
+                  ;; against the current base IRI of result."
+                  (and (relative-iri? value) (some? base-iri))
+                  (resolve-against base-iri value)
+                  ;; "(3.4.5) Otherwise, an invalid base IRI error has
+                  ;; been detected and processing is aborted."
+                  :else (throw (ex-info "invalid base IRI"
+                                        {:base-iri value})))}))
            ;; "(3.5) If context has an @vocab key:"
-           (when (contains? context "@vocab")
-             ;; TODO 3.5.1-3
-             )
+           (when-let [[_ value] (find context "@vocab")]
+             ;; "(3.5.1) Initialize value to the value associated with
+             ;; the @vocab key." (see above)
+             {:vocab
+              (cond
+                ;; "(3.5.2) If value is null, remove any vocabulary
+                ;; mapping from result."
+                (nil? value) nil
+                ;; "(3.5.3) Otherwise, if value is an absolute IRI
+                ;; or blank node identifier, the vocabulary mapping
+                ;; of result is set to value.
+                ((some-fn absolute-iri? blank-node-identifier?) value) value
+                ;; "...If it is not an absolute IRI or blank node
+                ;; identifier, an invalid vocab mapping error has
+                ;; been detected and processing is aborted."
+                :else (throw (ex-info "invalid vocab mapping"
+                                      {:vocab value})))})
            ;; "(3.6) If context has an @language key:"
-           (when (contains? context "@language")
-             ;; TODO 3.6.1-3
-             )))
+           (when-let [[_ value] (find context "@language")]
+             ;; "(3.6.1) Initialize value to the value associated with
+             ;; the @language key." (see above)
+             {:language
+              (cond
+                ;; "(3.6.2) If value is null, remove any default
+                ;; language from result."
+                (nil? value) nil
+                ;; "(3.6.3) Otherwise, if value is string, the
+                ;; default language of result is set to lowercased
+                ;; value."
+                (string? value) (str/lower-case value)
+                ;; "...If it is not a string, an invalid default
+                ;; language error has been detected and processing
+                ;; is aborted."
+                :else (throw (ex-info
+                              "invalid default language: unexpected type"
+                              {:language value})))})))
         ;; "(4) Return result."
         result))))
 
@@ -139,3 +203,27 @@
                   nil))
   ([active-context local-context document-iri]
    (contextualize* active-context local-context document-iri #{})))
+
+(defn create-term-definition
+  "https://www.w3.org/TR/json-ld-api/#h3_create-term-definition"
+  ;; "(§ 6.2) Create Term Definition – This algorithm is called from
+  ;; the Context Processing algorithm to create a term definition in
+  ;; the active context for a term being processed in a local
+  ;; context."
+
+  ;; returns: a new active context, a new `defined` (recursion),
+  ;; [others?] 
+  [active-context local-context term defined]
+
+  ;; "(1) If defined contains the key term..."
+  (if-let [[_ term-defined] (find defined term)]
+    ;; "...and the associated value is true (indicating that the term
+    ;; definition has already been created), return.
+    (if term-defined
+      ;; TODO return what?!
+      'todo
+      ;; Otherwise, if the value is false, a cyclic IRI mapping error
+      ;; has been detected and processing is aborted."
+      (throw (ex-info "cyclic IRI mapping"
+                      {;; TODO show the cycle
+                       })))))
